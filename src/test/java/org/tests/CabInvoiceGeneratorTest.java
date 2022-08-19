64644d63f5d0b1fe950639efd24c7290e7990441
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-19 21:02:06 +0530

    Added:Invoice Service

--- src/test/java/org/tests/CabInvoiceGeneratorTest.java
+++ src/test/java/org/tests/CabInvoiceGeneratorTest.java
@@ -2,6 +2,8 @@ package org.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+
 import org.junit.Before;
 import org.junit.Test;
 public class CabInvoiceGeneratorTest {
@@ -32,5 +34,28 @@ public class CabInvoiceGeneratorTest {
 		InvoiceSummery invoiceSummary = invoiceGenerator.calculateFare(rides);
 		InvoiceSummery expectedInvoiceSummary = new InvoiceSummery(2, 30);
 		assertEquals(expectedInvoiceSummary, invoiceSummary);
-}
+	}
+	@Test
+	public void givenRideRepository_whenGivenUserID_shouldReturnInvoice() {
+		HashMap<Integer, Ride[]> rideRepo = new HashMap<Integer, Ride[]>();
+
+		Ride[] ride1 = {new Ride(2.0 , 5) , new Ride(0.2 , 1)};
+		Ride[] ride2 = {new Ride(10.0 , 2) , new Ride(5.0 , 1)};
+		Ride[] ride3 = {new Ride(7.0 , 3) , new Ride(5.0 , 1)};
+		Ride[] ride4 = {new Ride(4.0 , 3) , new Ride(5.0 , 4)};
+
+		rideRepo.put(1, ride1);
+		rideRepo.put(2, ride2);
+		rideRepo.put(3, ride3);
+		rideRepo.put(4, ride4);
+
+		int userID = 1;
+
+		RideRepo rideRepository = new RideRepo();
+
+		InvoiceSummery invoiceSummary = rideRepository.calculateFare();       //actual output
+
+		InvoiceSummery expectedInvoiceSummary = new InvoiceSummery(2, 30.0);  //expected output as number of rides(2) in userID=1, total fare as 30.0. 
+		assertEquals(expectedInvoiceSummary, invoiceSummary);
+	}
 }
